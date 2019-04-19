91826699aa2d34f1bd47d59cedd139560e3f0e3b
Felipe de Aguiar Rodrigues Cembranelli <feliperc@cit>
2019-04-18 21:29:59 -0400

    adicionado arquivo raiz

---
+++ Demo-Aspnetcore/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace Demo_Aspnetcore
+{
+    /// <summary>
+    /// ammend a commit 1
+    /// ammend a commit 2
+    /// change original repogit
+    /// new push
+    /// </summary>
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        /// <summary>
+        /// comment method
+        /// </summary>
+        /// <param name="args"></param>
+        /// <returns></returns>
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
+    }
+}
---
+++ Demo-Aspnetcore/deploy_gcloud.bat
@@ -0,0 +1,13 @@
+﻿@echo on
+set gcloud_path="C:\Users\feliperc\AppData\Local\Google\Cloud SDK\google-cloud-sdk\bin\gcloud"
+
+echo GCLOUD_SERVICE_AUTH : %GCLOUD_SERVICE_AUTH%
+
+echo set auth
+call %gcloud_path% auth activate-service-account --key-file "C:\Program Files (x86)\Jenkins\workspace\DotNet-job-freestyle\Demo-Aspnetcore\jenkins-service-account4.json"
+
+echo set project
+call %gcloud_path% config set project "gcp-felipecembranelli2"
+
+echo Deploy
+call %gcloud_path% app deploy --quiet "%WORKSPACE%"\Demo-Aspnetcore\app.yaml
